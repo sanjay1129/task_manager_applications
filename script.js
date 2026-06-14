1ea3a5aacf246181da2430362202cc77625c4240
SANJAY <25ad129@rmkec.ac.in>
2026-06-14 14:08:45 +0000

    Task manager application

---
+++ script.js
@@ -0,0 +1,17 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+
+    if(input.value.trim() === ""){
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.innerHTML = `
+        ${input.value}
+        <button onclick="this.parentElement.remove()">Delete</button>
+    `;
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
